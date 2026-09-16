b5e4e70133e075acb8242f273e91b123f9394c57
nishtha-thakkar <nishthathakkar3@gmail.com>
2026-09-16 12:44:46 +0530

    portfolio

--- project2/src/pages/Projects.jsx
+++ project2/src/pages/Projects.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
 function Projects() {
-
   const project = [
     {
       title: "Portfolio Website",
       tech: "React JS, Tailwind CSS",
+      github: "https://github.com/nishtha-thakkar/react-portfolio",
     },
     {
       title: "Rapid Shop",
       tech: "HTML, CSS, JavaScript",
+      github:
+        "https://github.com/nishtha-thakkar/Javascript/tree/main/project/rapid-shop",
     },
+
     {
-      title: "Weather App",
-      tech: "JavaScript, REST API",
+      title: "E-commerce website",
+      tech: "Html, Css, react",
+      github: "https://github.com/nishtha-thakkar/react-e-commerce-website",
     },
   ];
 
@@ -23,7 +27,6 @@ function Projects() {
       className="bg-[#050B1F] text-white py-20 px-6 md:px-16"
     >
       <div className="max-w-7xl mx-auto">
-
         {/* Heading */}
         <h1 className="text-4xl md:text-5xl font-bold text-center mb-14">
           Projects
@@ -31,7 +34,6 @@ function Projects() {
 
         {/* Project Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-
           {project.map((item, index) => (
             <div
               key={index}
@@ -42,20 +44,20 @@ function Projects() {
                   {item.title}
                 </h2>
 
-                <p className="text-gray-300 mb-8">
-                  {item.tech}
-                </p>
+                <p className="text-gray-300 mb-8">{item.tech}</p>
               </div>
 
-              <button className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded-lg w-fit">
-                View Project
-              </button>
-
+              <a
+                href={item.github}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-500 hover:bg-blue-600 px-5 py-2 rounded-lg w-fit"
+              >
+                View Code
+              </a>
             </div>
           ))}
-
         </div>
-
       </div>
     </section>
   );
